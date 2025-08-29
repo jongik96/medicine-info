7e5dee764dad377879b579d7ae06f8d3de958551
jongik96 <pji3504@naver.com>
2025-08-29 15:34:58 +0900

    tooltip added

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,13 +30,13 @@ export default function Header() {
               <a href="#" className="text-gray-600 hover:text-primary transition-colors">
                 {t('header.contact')}
               </a>
-              <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-800 text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
-                {language === 'ja' 
-                  ? 'pji3503@gmail.com でお問い合わせください。'
-                  : 'Please contact us at pji3503@gmail.com'
-                }
-                <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-800"></div>
-              </div>
+                             <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-3 py-2 bg-gray-800 text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
+                 {language === 'ja' 
+                   ? 'pji3503@gmail.com でお問い合わせください。'
+                   : 'Please contact us at pji3503@gmail.com'
+                 }
+                 <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-b-4 border-transparent border-b-gray-800"></div>
+               </div>
             </div>
           </nav>
           
